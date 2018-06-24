4f2ae6dac23c72e9fbc087793a795a8ddfbfa447
Stefan <stefandjokic@pm.me>
2018-06-24 11:16:01 +0200

    added form controls

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.app') 
 @section('content')
-<h1>Create a post</h1>
+<h1>Publish a post</h1>
+
+<form method="POST" action="/posts">
+    <div class="form-group">
+      <label for="title">Title</label>
+      <input type="text" class="form-control" id="title" name="title">
+    </div>
+    <div class="form-group">
+      <label for="body">Body</label>
+      <textarea class="form-control" id="body" name="body"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
 @endsection

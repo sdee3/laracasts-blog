e7bd69f11b6557e770ce2b4a92a67c84c49cb819
Stefan <stefandjokic@pm.me>
2018-06-24 11:44:33 +0200

    post displaying modifications

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app') 
 @section('content')
 <h1>{{ $post->title }}</h1>
+<h4>Created on {{ $post->created_at->toFormattedDateString() }}</h4>
+<hr>
 <div>
   {!!$post->body!!}
 </div>

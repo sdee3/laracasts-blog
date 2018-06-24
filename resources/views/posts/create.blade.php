ddb50801caa5f1d367b2cc2300fb0fb83c1d2119
Stefan <stefandjokic@pm.me>
2018-06-24 11:21:37 +0200

    added csrf

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,6 +3,7 @@
 <h1>Publish a post</h1>
 
 <form method="POST" action="/posts">
+    {{ csrf_field() }}
     <div class="form-group">
       <label for="title">Title</label>
       <input type="text" class="form-control" id="title" name="title">
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::get('/posts', 'PostsController@index');
 // Route::get('/posts/{post}', 'PostsController@show');
 
 Route::get('/posts/create', 'PostsController@create');
+
+Route::post('/posts', 'PostsController@store');

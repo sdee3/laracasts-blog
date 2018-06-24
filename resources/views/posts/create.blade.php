c08926338c16355a542685f61c6859c0d6c4f1e8
Stefan <stefandjokic@pm.me>
2018-06-24 11:11:35 +0200

    removed unisharp/laravel-filemanager

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,19 +1,4 @@
 @extends('layouts.app') 
 @section('content')
 <h1>Create a post</h1>
-{!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
-
-  <div class="form-group">
-    {{ Form::label('title', 'Title') }}
-    {{ Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title']) }}
-  </div>
-
-  <div class="form-group">
-    {{ Form::label('body', 'Body') }}
-    {{ Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body']) }}
-  </div>
-
-  {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
-
-{!! Form::close() !!}
 @endsection

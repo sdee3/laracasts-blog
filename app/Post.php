74c7425c5af229d3b4d0a124f0ef5ef19b5a9213
Stefan <stefandjokic@pm.me>
2018-06-26 11:19:39 +0200

    connected user to comments and posts

--- app/Post.php
+++ app/Post.php
@@ -10,4 +10,9 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function user() 
+    {
+        return $this->belongsTo(User::class);
+    }
 }

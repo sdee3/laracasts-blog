9498bf2f6e15a115d26fb2132381e9352b75801b
Stefan <stefandjokic@pm.me>
2018-06-26 10:59:38 +0200

    added commenting on posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,5 +5,33 @@
 <hr>
 <div>
   {!!$post->body!!}
+  <hr>
+  <div class="comments">
+    <h5>Comments</h5>
+    <ul class="list-group">
+      @foreach ($post->comments as $comment)
+        <li class="list-group-item">
+          <strong>
+            {{ $comment->created_at->diffForHumans() }}: 
+          </strong>
+          {{ $comment->body }}
+        </li>
+      @endforeach
+    </ul>
+  </div>
+
+  <div class="card mt-5">
+    <div class="card-block">
+      <form method="POST" action="/posts/{{$post->id}}/comments">
+        {{ csrf_field() }}
+        <div class="form-group">
+          <textarea name="body" placeholder="Your comment here" class="form-control" required></textarea>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Add comment</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
 @endsection
